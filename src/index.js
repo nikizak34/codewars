6ee388f9fcc218cee2cf3b6efc21a6040da79a0e
nikizak34 <nikita2000.16.11@gmail.com>
2023-06-12 13:28:41 +0300

    '17'

--- src/index.js
+++ src/index.js
@@ -273,3 +273,17 @@ function reverseWords(str) {
 }
 
 console.log(reverseWords('double  spaced  words'))
+
+
+//17.
+//Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple
+// elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
+function removeSmallest(numbers) {
+    const min= Math.min(...numbers)
+   const min2= numbers.indexOf(min)
+    const min3= numbers.filter((el,index)=>index!==min2)
+  return min3
+}
+
+console.log(removeSmallest([2,2,1,2,1]))
